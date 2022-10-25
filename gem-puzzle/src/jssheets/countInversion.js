a589a2cb15b0407c4ce7baa1a1e288ed3e43e36f
Roman Lysiuk <gekalo1025@gmail.com>
2022-10-25 20:11:42 +0300

    fix: Changed the algorithm for calculating inversions

--- gem-puzzle/src/jssheets/countInversion.js
+++ gem-puzzle/src/jssheets/countInversion.js
@@ -1,8 +1,9 @@
 export default function countInversion(array) {
+  const newArr = array.filter((el)=> el != 0);
   let countInversion = 0;
-  for (let i = 0; i < array.length; i++) {
-    for (let j = i; j < array.length; j++) {
-      if (array[i] <= array[j]) {
+  for (let i = 0; i < newArr.length; i++) {
+    for (let j = i; j < newArr.length; j++) {
+      if (newArr[i] <= newArr[j]) {
       } else {
         countInversion++;
       }
